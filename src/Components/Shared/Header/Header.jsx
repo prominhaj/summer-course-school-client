9d981cf570aeb3db906d6e6eb03d2c11d8a68da8
Md Minhaj <parsonal322532@gmail.com>
2024-02-08 04:00:10 +0600

    Add Firebase Config File

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -6,56 +6,6 @@ import { Link, NavLink } from "react-router-dom";
 import { FormControlLabel, Switch, styled } from "@mui/material";
 
 // Switch
-// const IOSSwitch = styled((props) => (
-//   <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-// ))(({ theme }) => ({
-//   width: 42,
-//   height: 26,
-//   padding: 0,
-//   "& .MuiSwitch-switchBase": {
-//     padding: 0,
-//     margin: 2,
-//     transitionDuration: "300ms",
-//     "&.Mui-checked": {
-//       transform: "translateX(16px)",
-//       color: "#fff",
-//       "& + .MuiSwitch-track": {
-//         backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
-//         opacity: 1,
-//         border: 0,
-//       },
-//       "&.Mui-disabled + .MuiSwitch-track": {
-//         opacity: 0.5,
-//       },
-//     },
-//     "&.Mui-focusVisible .MuiSwitch-thumb": {
-//       color: "#33cf4d",
-//       border: "6px solid #fff",
-//     },
-//     "&.Mui-disabled .MuiSwitch-thumb": {
-//       color:
-//         theme.palette.mode === "light"
-//           ? theme.palette.grey[100]
-//           : theme.palette.grey[600],
-//     },
-//     "&.Mui-disabled + .MuiSwitch-track": {
-//       opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
-//     },
-//   },
-//   "& .MuiSwitch-thumb": {
-//     boxSizing: "border-box",
-//     width: 22,
-//     height: 22,
-//   },
-//   "& .MuiSwitch-track": {
-//     borderRadius: 26 / 2,
-//     backgroundColor: theme.palette.mode === "light" ? "#374151" : "#39393D",
-//     opacity: 1,
-//     transition: theme.transitions.create(["background-color"], {
-//       duration: 500,
-//     }),
-//   },
-// }));
 const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   width: 62,
   height: 34,
@@ -188,10 +138,6 @@ const Header = () => {
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   <button onClick={toggleButton}>
-                    {/* <FormControlLabel
-                      control={<IOSSwitch sx={{ m: -1 }} checked={checked} />}
-                      ref={toggleRef}
-                    /> */}
                     <FormControlLabel
                       control={
                         <MaterialUISwitch sx={{ m: 1 }} checked={checked} />
---
+++ src/Provider/AuthContext.jsx
@@ -0,0 +1,10 @@
+
+const AuthContext = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthContext;
