ef5cbf0730d71bf956c0c8d19ff883301f97a7c3
Md Minhaj <parsonal322532@gmail.com>
2024-02-12 04:23:25 +0600

    Implement Instructors page using Pagination

--- src/Components/AllInstructorsCard/AllInstructorsCard.jsx
+++ src/Components/AllInstructorsCard/AllInstructorsCard.jsx
@@ -5,7 +5,7 @@ const AllInstructorsCard = ({ item }) => {
   const { category, email, name, image, totalCourse, _id } = item;
 
   return (
-    <div className="flex flex-col cursor-pointer duration-300 border bg-[#3D70E4] md:rounded-lg dark:bg-black shadow-xl  dark:shadow-gray-700 dark:border-gray-700 hover:scale-[1.03] rounded-xl">
+    <div className="flex flex-col duration-500 cursor-pointer border bg-[#F8FAFC] md:rounded-lg dark:bg-black shadow-xl  dark:shadow-gray-700 dark:border-gray-700 hover:scale-[1.03] rounded-xl">
       <div className="relative flex flex-col items-center justify-center gap-2 p-5 rounded-br-none">
         <div>
           <img
@@ -14,7 +14,7 @@ const AllInstructorsCard = ({ item }) => {
             alt="Instructors Image"
           />
         </div>
-        <h3 className="block text-base font-semibold leading-none tracking-tight text-gray-200 sm:text-lg">
+        <h3 className="block text-base font-semibold leading-none tracking-tight text-gray-800 dark:text-gray-300 sm:text-lg">
           {category}
         </h3>
         <h4 className="absolute p-4 font-semibold leading-3 text-gray-200 bg-gray-400 rounded-full sm:p-5 md:leading-3 md:text-xl dark:bg-gray-700 sm:top-5 sm:right-8 top-3 right-5">
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-import useLoadData from "../../Hooks/useLoadData/useLoadData";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
-import InstructorsCard from "../../Components/InstructorsCard/InstructorsCard";
 import AllInstructorsCard from "../../Components/AllInstructorsCard/AllInstructorsCard";
+import axios from "axios";
+import useLoadData from "../../Hooks/useLoadData/useLoadData";
+import { Pagination } from "@mui/material";
+import CardLoading from "../../Components/CardLoading/CardLoading";
 
 const Instructors = () => {
-  const [data, dataLoading] = useLoadData("/all-instructors");
-  const { allCategory, allInstructors } = data;
+  const [instructors, setInstructors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Pagination
+  const [data] = useLoadData("/total-instructors-count");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [instructorsPerPage] = useState(6);
+  const totalPages = Math.ceil(data?.totalDataCount / instructorsPerPage);
+
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchPosts = async () => {
+      const res = await axios.get(
+        `http://localhost:3000/all-instructors?page=${currentPage}&limit=${instructorsPerPage}`
+      );
+      setInstructors(res?.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, [currentPage, instructorsPerPage]);
+
+  const { allCategory, allInstructors } = instructors;
 
   return (
     <main className="bg-white dark:bg-[#0E1528] py-5">
@@ -48,41 +73,27 @@ const Instructors = () => {
             </div>
           </div>
           <section className="flex-1 w-full">
-            <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-              {allInstructors?.map((item, index) => (
-                <AllInstructorsCard
-                  key={item._id}
-                  item={item}
-                />
-              ))}
-            </div>
-
-            {/* <nav
-              className="inline-flex -space-x-px rounded-md shadow-sm isolate"
-              aria-label="Pagination"
-            >
-              <button
-                href="#"
-                className="relative inline-flex items-center px-2 py-2 text-gray-400 rounded-l-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-              <a
-                className="relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold text-white bg-indigo-600 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                1
-              </a>
-              <a
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                2
-              </a>
-              <button className="relative inline-flex items-center px-2 py-2 text-gray-400 rounded-r-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-            </nav> */}
+            {loading ? (
+              <CardLoading />
+            ) : (
+              <>
+                <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+                  {allInstructors?.map((item) => (
+                    <AllInstructorsCard key={item._id} item={item} />
+                  ))}
+                </div>
+                <div className="pt-8">
+                  <Pagination
+                    onChange={handleChange}
+                    className="flex justify-center py-2 bg-gray-300 rounded-md"
+                    size="large"
+                    page={currentPage}
+                    count={totalPages || 1}
+                    color="primary"
+                  />
+                </div>
+              </>
+            )}
           </section>
         </section>
       </div>
