b7ab568630c045c7269cf811b47b6b76a5e8c381
Md Minhaj <parsonal322532@gmail.com>
2024-02-17 12:03:59 +0600

    Implement User Side Complete

--- src/Pages/CourseDashBoard/CourseDashBoard/CourseDashBoard.jsx
+++ src/Pages/CourseDashBoard/CourseDashBoard/CourseDashBoard.jsx
@@ -4,12 +4,12 @@ import { useLoaderData } from "react-router-dom";
 
 const CourseDashBoard = () => {
   const data = useLoaderData();
-  console.log(data);
   const { image, name, category, details, enrollEmail } = data;
+
   return (
     <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200">
       <div className="container px-5 py-5 mx-auto md:py-10">
-        <div className="grid gap-5 md:grid-cols-2">
+        <div className="grid gap-5 lg:grid-cols-2">
           <div>
             <img
               className="w-full h-[500px] object-cover rounded-lg"
@@ -18,21 +18,28 @@ const CourseDashBoard = () => {
             />
           </div>
           <div>
-            <h2>{name}</h2>
-            <h2>{category}</h2>
-            <p>
-              <small>{details}</small>
-            </p>
-            <div className="flex">
-              <AvatarGroup max={4}>
-                {enrollEmail.map((item) => (
-                  <Avatar
-                    key={item}
-                    alt={item?.substr(0, 1)}
-                    src={item?.substr(0, 1)}
-                  />
-                ))}
-              </AvatarGroup>
+            <div className="space-y-3">
+              <h2 className="text-xlz md:text-2xl">{name}</h2>
+              <h2 className="text-lg md:text-xl">{category}</h2>
+              <p>
+                <small>{details}</small>
+              </p>
+              <div className="flex">
+                <AvatarGroup max={4}>
+                  {enrollEmail.map((item) => (
+                    <Avatar
+                      key={item}
+                      alt={item?.substr(0, 1)}
+                      src={item?.substr(0, 1)}
+                    />
+                  ))}
+                </AvatarGroup>
+              </div>
+            </div>
+            <div className="mt-16">
+              <h4 className="text-2xl font-medium text-center">
+                Update Next Time
+              </h4>
             </div>
           </div>
         </div>
